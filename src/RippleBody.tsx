3951c571637e29a2c080bcc90835b28e700c66e4
no <no>
2020-12-29 19:33:19 +0200

    init

---
+++ src/RippleBody.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+export interface IRippleBodyProps {
+    acive: boolean;
+    onEnd: () => void;
+}
+export const RippleBody: FC<IRippleBodyProps> = () => {
+    <div></div>;
+};
